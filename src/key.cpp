b3ad165479e85c1228f5d1e344ec40ee3ade9c17
mraksoll <32709596+mraksoll4@users.noreply.github.com>
2024-02-29 03:42:50 +0200

    Update

--- src/key.cpp
+++ src/key.cpp
@@ -244,7 +244,7 @@ bool CKey::VerifyPubKey(const CPubKey& pubkey) const {
         return false;
     }
     unsigned char rnd[8];
-    std::string str = "Bitcoin key verification\n";
+    std::string str = "Bewcore key verification\n";
     GetRandBytes(rnd);
     uint256 hash{Hash(str, rnd)};
     std::vector<unsigned char> vchSig;
