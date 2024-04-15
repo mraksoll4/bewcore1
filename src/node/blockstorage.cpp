53b4711801313086c9fc7d7fa8d0e1f976f4734a
someunknownman <166449787+someunknownman@users.noreply.github.com>
2024-04-15 04:00:35 +0300

    Update blockstorage.cpp

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -120,11 +120,11 @@ bool BlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, s
                 pindexNew->nNonce         = diskindex.nNonce;
                 pindexNew->nStatus        = diskindex.nStatus;
                 pindexNew->nTx            = diskindex.nTx;
-
+                /*
                 if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams)) {
                     return error("%s: CheckProofOfWork failed: %s", __func__, pindexNew->ToString());
                 }
-
+                */
                 pcursor->Next();
             } else {
                 return error("%s: failed to read value", __func__);
