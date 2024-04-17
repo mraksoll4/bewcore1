4d1f410d1c12571f03fafe2303e76bbef16ef812
someunknownman <166449787+someunknownman@users.noreply.github.com>
2024-04-17 21:13:30 +0300

    Update chainparams.cpp

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -487,7 +487,7 @@ public:
                 .height = 110,
                 .hash_serialized = AssumeutxoHash{uint256S("0x6657b736d4fe4db0cbc796789e812d5dba7f5c143764b1b6905612f1830609d1")},
                 .nChainTx = 111,
-                .blockhash = uint256S("0x493b0c32c121d060458ffa5fe400aa5828afc5e0dd1a74654ac9c05bbe8dca15")
+                .blockhash = uint256S("0xd15210e4aa854744051a2f169650ce824a57d97f0a27ab3829bfac94308e77ab")
             },
             {
                 // For use by test/functional/feature_assumeutxo.py
