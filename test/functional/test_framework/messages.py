837a4764eb9ca62a74d2161f97ea5bf11d4b6c83
someunknownman <166449787+someunknownman@users.noreply.github.com>
2024-04-18 03:34:43 +0300

    Update messages.py

--- test/functional/test_framework/messages.py
+++ test/functional/test_framework/messages.py
@@ -745,7 +745,12 @@ class CBlockHeader:
             r += struct.pack("<I", self.nNonce)
             self.sha256 = uint256_from_str(hash256(r))
             self.hash = hash256(r)[::-1].hex()
-            self.argon2id = uint256_from_str(GetArgon2idHash(GetArgon2idHash(r, hashlib.sha512(hashlib.sha512(r).digest()).digest(), 4096), r, 32768)).digest()[::-1].hex()
+            # Получение хеша Argon2id дважды
+            hash1 = GetArgon2idHash(r, hashlib.sha512(hashlib.sha512(r).digest()).digest(), 4096)
+            hash2 = GetArgon2idHash(hash1, r, 32768)
+            
+            # Преобразование хеша в uint256 и получение его шестнадцатеричного представления
+            self.argon2id = uint256_from_str(hash2).digest()[::-1].hex()
 
     def rehash(self):
         self.sha256 = None
